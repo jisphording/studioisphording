df4891a4d01334047cbbae8554f21e03a9b0c794
johannesisphording <johannes@studioisphording.com>
2017-07-11 19:00:40 +0200

    [setup] adding php includes to the site to kinda work as a "cms light"

---
+++ inc/footer_projects.php
@@ -0,0 +1,11 @@
+</body><!-- closing body which is opened in header include-->
+
+<footer>
+    <div class="contact">
+        <p>Studio Isphording</p>
+        <p>017626197704</p>
+        <p>Prinzessinnenstraße 16</p>
+        <p>10969 Berlin</p>
+        <p>Copyright Studio Isphording 2012-2017</p>
+    </div>
+</footer>
---
+++ inc/header_projects.php
@@ -0,0 +1,28 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Studio Isphording</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- Import Fonts from Google -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:700|Playfair+Display:400,400i,900" rel="stylesheet">
+
+    <!-- <link rel="stylesheet" href="css/normalize.css"> -->
+    <link rel="stylesheet" href="../css/main.css">
+     
+    <script src="..js/vendor/modernizr-2.8.3.min.js"></script>
+</head> 
+<body><!-- opening body which is closed in footer include -->
+    
+    <nav>
+        <img src="../img/stempel.gif" class="logo" alt="logo" width="auto" height="auto">
+        <ul>
+            <li><a href="../index.php">Home</a></li>
+            <li><a href="../projects/pantrykitchen.php">Pantry Kitchen</a></li>
+            <li><a href="../contact.php">Contact</a></li>
+        </ul>
+    </nav>
