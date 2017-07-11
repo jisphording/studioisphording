1b1b786d7702516e7ee00616e1dd90ab0a0d6fb4
johannesisphording <johannes@studioisphording.com>
2017-07-11 18:59:09 +0200

    [setup] introducing a contact page

---
+++ contact.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html class="no-js" lang="">
+    
+    <?php include 'inc/header.php'; ?>  
+
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        <img src="img/stempel.gif" alt="Studio Isphording Logo" height="1280" width="1280">
+        <p>Feel Free to contact us for inquiries.</p>
+        
+        <section id="contact">
+            
+            <div class="contact">
+                <p>017626197704</p>
+                <a href="&#x6d;&#97;&#105;&#108;&#x74;&#x6f;&#58;&#x6a;&#111;&#104;&#x61;&#110;&#110;&#101;&#x73;&#64;&#x73;&#116;&#x75;&#x64;&#105;&#111;&#x69;&#115;&#x70;&#104;&#x6f;&#114;&#x64;&#105;&#x6e;&#x67;&#46;&#x63;&#x6f;&#109;">&#106;&#x6f;&#104;&#97;&#110;&#x6e;&#101;&#x73;&#x40;&#115;&#x74;&#117;&#x64;&#105;&#x6f;&#x69;&#x73;&#112;&#x68;&#x6f;&#x72;&#100;&#105;&#x6e;&#103;&#x2e;&#x63;&#x6f;&#x6d;</a><!-- encrypted to avoid spam -->
+                <p>Prinzessinnenstraße 16</p>
+                <p>10969 Berlin</p>
+            </div>
+            
+        </section>
+
+    <?php include $_SERVER['DOCUMENT_ROOT']."/studioisphording/inc/footer_projects.php"; ?>
+    
+</html>
