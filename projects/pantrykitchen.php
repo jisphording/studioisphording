8dfb0453866234b765fe2ca7ba13331551e6bade
johannesisphording <johannes@studioisphording.com>
2017-07-11 18:59:46 +0200

    [setup] introducing project pages and adding pantrykitchen project to the site

---
+++ projects/pantrykitchen.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html class="no-js" lang="">
+    
+    <?php include $_SERVER['DOCUMENT_ROOT']."/studioisphording/inc/header_projects.php"; ?> 
+    
+        <section class="project">
+            
+            <!-- Add your site or application content here -->
+            <p>Project Title</p>
+            <h2>Pantry Kitchen</h2>
+            <img src="../img/projects/pantrykitchen-01.jpg" alt="Pantrykitchen" height="auto" width="auto">
+            <img src="../img/projects/pantrykitchen-02.jpg" alt="Pantrykitchen" height="auto" width="auto">
+            <p>This pantry kitchen is a small architectural interior visualization project. It was done as an exercise in modeling, asset creation and rendering and was realized using Cinema 4D and VRay as a rendering engine.</p>
+        
+        </section>
+    
+    <?php include $_SERVER['DOCUMENT_ROOT']."/studioisphording/inc/footer_projects.php"; ?>
+    
+</html>
