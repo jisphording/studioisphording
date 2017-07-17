503372e14240d92ff717b46167196c7fbed84304
johannesisphording <johannes@studioisphording.com>
2017-07-18 01:45:59 +0200

    [setup] added an about page

---
+++ about.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html class="no-js" lang="">
+    
+    <?php include 'inc/header.php'; ?>  
+
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        <img src="img/stempel.gif" alt="Studio Isphording Logo" height="1280" width="1280">
+        
+        <section class="about">
+            <h2 class="iridian">Get t know us.</h2>
+            <p>Studio Isphording is a design practice in Berlin. We are working mainly in the digital realm, but coming from we've had our fair share of classic graphic design. Yes, including shitloads of posters and business reports.</p>
+            <p>Striving to make the complex clear &amp; beautiful we are working since 2012 and already had the privilege of having been involved on projects that have been awarded the Red Dot and IF awards.</p>
+        </section>
+    
+        <section class="credits">
+                <div class="credits-lists">
+                    <ul class="credits-list">
+                        <li class="credits-title">Client</li>
+                        <li class="credits-credit">Axel Springer</li>
+                        <li class="credits-credit">BMW</li>
+                        <li class="credits-credit">Boon</li>
+                        <li class="credits-credit">Büro + Staubach</li>
+                        <li class="credits-credit">Conway</li>
+                        <li class="credits-credit">CRRC</li>
+                        <li class="credits-credit">Deutsche Grammophon</li>
+                        <li class="credits-credit">Dickersbach Kunstverlag</li>
+                        <li class="credits-credit">ERGO</li>
+                    </ul>
+                    <ul class="credits-list">
+                        <li class="credits-title">Client</li>
+                        <li class="credits-credit">ESCP Europe</li>
+                        <li class="credits-credit">Four Music</li>
+                        <li class="credits-credit">Hubraum</li>
+                        <li class="credits-credit">Intercontinental Hotels</li>
+                        <li class="credits-credit">Sony Music</li>
+                        <li class="credits-credit">Telekom</li>
+                        <li class="credits-credit">Universal Music</li>
+                        <li class="credits-credit">Universität St. Gallen</li>
+                    </ul>
+                    <ul class="credits-list">
+                        <li class="credits-title">Client</li>
+                        <li class="credits-credit">Vattenfall</li>
+                        <li class="credits-credit">Vaola</li>
+                        <li class="credits-credit">VDA</li>
+                        <li class="credits-credit">Victoria Fahrräder</li>
+                        <li class="credits-credit">Voestalpine</li>
+                        <li class="credits-credit">Volkswagen</li>
+                        <li class="credits-credit">Warner Music</li>
+                        <li class="credits-credit">Welt N24</li>
+                        <li class="credits-credit">Wirecard</li>
+                    </ul>
+                </div>
+                <div class="credits-links">
+                    <ul class="credits-list credits-list--end">
+                        <li class="credits-title">Links</li>
+                        <li class="credits-link"><a target="_blank" href="index.php">Back to Home <img class="credits-arrow" src="../img/site/right-arrow.svg"></a></li>
+                    </ul>
+                </div>
+        </section>
+    
+        <section class="findmeon index">
+            <ul>
+                <li><a href="https://www.instagram.com/studioisphording/">Instagram</a></li>
+                <li><a href="https://dribbble.com/isphording">Dribble</a></li>
+                <li><a href="https://twitter.com/jisphording">Twitter</a></li>
+                <li><a href="https://www.linkedin.com/in/johannes-isphording-622033139/">LinkedIn</a></li>
+            </ul>
+        </section>
+        
+        <!-- Add your site or application content here
+        <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. 
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='https://www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script> -->
+
+    <?php include $_SERVER['DOCUMENT_ROOT']."/inc/footer_projects.php"; ?>
+    
+</html>
