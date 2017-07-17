d7bdc3482b7103f8c1e67ac4553f46c26131352e
johannesisphording <johannes@studioisphording.com>
2017-07-18 01:46:34 +0200

    [content] added two new projects to the site

---
+++ projects/altbau.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html class="no-js" lang="">
+    
+    <?php include $_SERVER['DOCUMENT_ROOT']."/inc/header_projects.php"; ?> 
+    
+        <section class="project, altbau">
+            
+            <!-- Add your site or application content here -->
+            <h3>Project Title</h3>
+            <h2 class="iridian">Altbau</h2>
+            
+            <ul class="project-gallery">
+                <li><img src="../img/projects/altbau-01.jpg" alt="Altbau" height="auto" width="auto"></li>
+                <li><img src="../img/projects/altbau-02.jpg" alt="Altbau" height="auto" width="auto"></li>
+            </ul>
+            
+            <p class="bigbold">This altbau kitchen features SieMatic and is an exercise in monochrome restraint.</p>
+        
+        </section>
+    
+        <section class="credits">
+                <div class="credits-lists">
+                    <ul class="credits-list">
+                        <li class="credits-title">Details</li>
+                        <li class="credits-credit">Architectural Visualization</li>
+                        <li class="credits-credit">Team: Studio Isphording</li>
+                        <li class="credits-credit">Year: 2017</li>
+                    </ul>
+                    <ul class="credits-list">
+                        <li class="credits-title">Primary Roles</li>
+                        <li class="credits-credit">Modeling</li>
+                        <li class="credits-credit">Texturing</li>
+                        <li class="credits-credit">Lighting &amp; Rendering</li>
+                    </ul>
+                    <ul class="credits-list">
+                        <li class="credits-title">Project Type</li>
+                        <li class="credits-credit">Interior</li>
+                        <li class="credits-credit">Architecture</li>
+                        <li class="credits-credit">Visualization</li>
+                    </ul>
+                </div>
+                <div class="credits-links">
+                    <ul class="credits-list credits-list--end">
+                        <li class="credits-title">Links</li>
+                        <li class="credits-link"><a target="_blank" href="../index.php">Visit site <img class="credits-arrow" src="../img/site/right-arrow.svg"></a></li>
+                    </ul>
+                </div>
+        </section>
+    
+        <section class="next-project">
+            <a href="pantrykitchen.php">Next Project</a>
+        </section>
+        <section class="findmeon">
+            <ul>
+                <li><a href="https://www.instagram.com/studioisphording/">Instagram</a></li>
+                <li><a href="https://dribbble.com/isphording">Dribble</a></li>
+                <li><a href="https://twitter.com/jisphording">Twitter</a></li>
+                <li><a href="https://www.linkedin.com/in/johannes-isphording-622033139/">LinkedIn</a></li>
+            </ul>
+        </section>
+    
+    <?php include $_SERVER['DOCUMENT_ROOT']."/inc/footer_projects.php"; ?>
+    
+</html>
---
+++ projects/eames_dsw.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html class="no-js" lang="">
+    
+    <?php include $_SERVER['DOCUMENT_ROOT']."/inc/header_projects.php"; ?> 
+    
+        <section class="project, eames">
+            
+            <!-- Add your site or application content here -->
+            <h3>Project Title</h3>
+            <h2 class="iridian">Eames DSW Chair</h2>
+            <ul class="project-gallery">
+                <li><img src="../img/projects/eames_dsw-01.jpg" alt="Eames DSW" height="auto" width="auto"></li>
+                <li><img src="../img/projects/eames_dsw-02.jpg" alt="Eames DSW" height="auto" width="auto"></li>
+            </ul>
+            
+            <p class="bigbold">This iconic midcentury chair is one of our favourite Eames Designs and so it was perfect for a small studio rendering scenario.</p>
+        
+        </section>
+    
+        <section class="credits">
+                <div class="credits-lists">
+                    <ul class="credits-list">
+                        <li class="credits-title">Details</li>
+                        <li class="credits-credit">Product Visualization</li>
+                        <li class="credits-credit">Team: Studio Isphording</li>
+                        <li class="credits-credit">Year: 2017</li>
+                    </ul>
+                    <ul class="credits-list">
+                        <li class="credits-title">Primary Roles</li>
+                        <li class="credits-credit">Texturing</li>
+                        <li class="credits-credit">Lighting &amp; Rendering</li>
+                    </ul>
+                    <ul class="credits-list">
+                        <li class="credits-title">Project Type</li>
+                        <li class="credits-credit">Product</li>
+                        <li class="credits-credit">Furniture</li>
+                        <li class="credits-credit">Visualization</li>
+                    </ul>
+                </div>
+                <div class="credits-links">
+                    <ul class="credits-list credits-list--end">
+                        <li class="credits-title">Links</li>
+                        <li class="credits-link"><a target="_blank" href="../index.php">Back to Home <img class="credits-arrow" src="../img/site/right-arrow.svg"></a></li>
+                    </ul>
+                </div>
+        </section>
+    
+        <section class="next-project">
+            <a href="altbau.php">Next Project</a>
+        </section>
+        <section class="findmeon">
+            <ul>
+                <li><a href="https://www.instagram.com/studioisphording/">Instagram</a></li>
+                <li><a href="https://dribbble.com/isphording">Dribble</a></li>
+                <li><a href="https://twitter.com/jisphording">Twitter</a></li>
+                <li><a href="https://www.linkedin.com/in/johannes-isphording-622033139/">LinkedIn</a></li>
+            </ul>
+        </section>
+    
+    <?php include $_SERVER['DOCUMENT_ROOT']."/inc/footer_projects.php"; ?>
+    
+</html>
