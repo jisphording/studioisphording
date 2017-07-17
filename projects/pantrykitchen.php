9c6f17295d4ac5fda3335629a64fa3485ca20bff
johannesisphording <johannes@studioisphording.com>
2017-07-17 21:06:13 +0200

    [content] credits block was added to the project page for additional information and better collaboration overview. Nextproject and Find Me On were added in project footer.

--- projects/pantrykitchen.php
+++ projects/pantrykitchen.php
@@ -3,15 +3,65 @@
     
     <?php include $_SERVER['DOCUMENT_ROOT']."/inc/header_projects.php"; ?> 
     
-        <section class="project">
+        <section class="project, pantry-kitchen">
             
             <!-- Add your site or application content here -->
             <p>Project Title</p>
             <h2>Pantry Kitchen</h2>
+            <!-- Embed Responsively - Vimeo -->
+            <div class='embed-container'><iframe src='https://player.vimeo.com/video/72260648' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
+            
             <img src="../img/projects/pantrykitchen-01.jpg" alt="Pantrykitchen" height="auto" width="auto">
             <img src="../img/projects/pantrykitchen-02.jpg" alt="Pantrykitchen" height="auto" width="auto">
+            
             <p>This pantry kitchen is a small architectural interior visualization project. It was done as an exercise in modeling, asset creation and rendering and was realized using Cinema 4D and VRay as a rendering engine.</p>
+            
+            <img src="../img/projects/pantrykitchen-03.jpg" alt="Pantrykitchen" height="auto" width="auto">
+            <img src="../img/projects/pantrykitchen-04.jpg" alt="Pantrykitchen" height="auto" width="auto">
+            
+            <p class="bigbold">This pantry kitchen is a small architectural interior visualization project. It was done as an exercise in modeling, asset creation and rendering and was realized using Cinema 4D and VRay as a rendering engine.</p>
         
+        </section>
+    
+        <section class="credits">
+                <div class="credits-lists">
+                    <ul class="credits-list">
+                        <li class="credits-title">Details</li>
+                        <li class="credits-credit">Architectural Visualization</li>
+                        <li class="credits-credit">Team: Studio Isphording</li>
+                        <li class="credits-credit">Year: 2017</li>
+                    </ul>
+                    <ul class="credits-list">
+                        <li class="credits-title">Primary Roles</li>
+                        <li class="credits-credit">Modeling</li>
+                        <li class="credits-credit">Texturing</li>
+                        <li class="credits-credit">Lighting &amp; Rendering</li>
+                    </ul>
+                    <ul class="credits-list">
+                        <li class="credits-title">Project Type</li>
+                        <li class="credits-credit">Interior</li>
+                        <li class="credits-credit">Architecture</li>
+                        <li class="credits-credit">Visualization</li>
+                    </ul>
+                </div>
+                <div class="credits-links">
+                    <ul class="credits-list credits-list--end">
+                        <li class="credits-title">Links</li>
+                        <li class="credits-link"><a target="_blank" href="#">Visit site <img class="credits-arrow" src="../img/site/right-arrow.svg"></a></li>
+                    </ul>
+                </div>
+            </section>
+    
+        <section class="next-project">
+            <a href="#">Next Project</a>
+        </section>
+        <section class="findmeon">
+            <ul>
+                <li><a href="https://www.instagram.com/studioisphording/">Instagram</a></li>
+                <li><a href="https://dribbble.com/isphording">Dribble</a></li>
+                <li><a href="https://twitter.com/jisphording">Twitter</a></li>
+                <li><a href="https://www.linkedin.com/in/johannes-isphording-622033139/">LinkedIn</a></li>
+            </ul>
         </section>
     
     <?php include $_SERVER['DOCUMENT_ROOT']."/inc/footer_projects.php"; ?>
