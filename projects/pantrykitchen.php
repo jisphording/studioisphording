bf000412d10fe933963cb00fb14c927091c452c1
johannesisphording <johannes@studioisphording.com>
2017-07-18 01:47:52 +0200

    [structure] moved project images into a list for better layout control

--- projects/pantrykitchen.php
+++ projects/pantrykitchen.php
@@ -6,20 +6,22 @@
         <section class="project, pantry-kitchen">
             
             <!-- Add your site or application content here -->
-            <p>Project Title</p>
-            <h2>Pantry Kitchen</h2>
-            <!-- Embed Responsively - Vimeo -->
-            <div class='embed-container'><iframe src='https://player.vimeo.com/video/72260648' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
+            <h3>Project Title</h3>
+            <h2 class="iridian">Pantry Kitchen</h2>
+            <!-- Embed Responsively - Vimeo
+            <div class='embed-container'><iframe src='https://player.vimeo.com/video/72260648' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div> -->
             
-            <img src="../img/projects/pantrykitchen-01.jpg" alt="Pantrykitchen" height="auto" width="auto">
-            <img src="../img/projects/pantrykitchen-02.jpg" alt="Pantrykitchen" height="auto" width="auto">
+            <ul class="project-gallery">
+                <li><img src="../img/projects/pantrykitchen-01.jpg" alt="Pantrykitchen" height="auto" width="auto"></li>
+                <li><img src="../img/projects/pantrykitchen-02.jpg" alt="Pantrykitchen" height="auto" width="auto"></li>
+            </ul>
             
             <p>This pantry kitchen is a small architectural interior visualization project. It was done as an exercise in modeling, asset creation and rendering and was realized using Cinema 4D and VRay as a rendering engine.</p>
             
-            <img src="../img/projects/pantrykitchen-03.jpg" alt="Pantrykitchen" height="auto" width="auto">
-            <img src="../img/projects/pantrykitchen-04.jpg" alt="Pantrykitchen" height="auto" width="auto">
-            
-            <p class="bigbold">This pantry kitchen is a small architectural interior visualization project. It was done as an exercise in modeling, asset creation and rendering and was realized using Cinema 4D and VRay as a rendering engine.</p>
+            <ul class="project-gallery">
+                <li><img src="../img/projects/pantrykitchen-03.jpg" alt="Pantrykitchen" height="auto" width="auto"></li>
+                <li><img src="../img/projects/pantrykitchen-04.jpg" alt="Pantrykitchen" height="auto" width="auto"></li>
+            </ul>
         
         </section>
     
@@ -47,13 +49,13 @@
                 <div class="credits-links">
                     <ul class="credits-list credits-list--end">
                         <li class="credits-title">Links</li>
-                        <li class="credits-link"><a target="_blank" href="#">Visit site <img class="credits-arrow" src="../img/site/right-arrow.svg"></a></li>
+                        <li class="credits-link"><a target="_blank" href="../index.php">Back to Home <img class="credits-arrow" src="../img/site/right-arrow.svg"></a></li>
                     </ul>
                 </div>
-            </section>
+        </section>
     
         <section class="next-project">
-            <a href="#">Next Project</a>
+            <a href="eames_dsw.php">Next Project</a>
         </section>
         <section class="findmeon">
             <ul>
