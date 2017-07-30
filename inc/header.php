6a3f94f89af6484fa0ea85d8e5c3dc97538b8296
Johannes Isphording <mail@johannesisphording.de>
2017-07-30 21:06:20 +0200

    [content] adding a dedicated services page to the site to make it clear what we do without talking to much about ourselves.

--- inc/header.php
+++ inc/header.php
@@ -23,6 +23,7 @@
         <ul>
             <li><a href="index.php">Home</a></li>
             <li><a href="about.php">About</a></li>
+            <li><a href="services.php">Services</a></li>
             <li><a href="contact.php">Contact</a></li>
         </ul>
     </nav>
---
+++ services.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html class="no-js" lang="">
+    
+    <?php include 'inc/header.php'; ?>  
+
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->  
+        <section class="services">
+            <h2 class="iridian">What we can do for you.</h2>
+            <p><span class="iridian">In short: </span>Our main areas of work are 3D Visualization and Interfacedesign.</p>
+            
+            <p><span class="iridian">With a little more fluff: </span>At Studio Isphording we believe that Design as a practice is becoming broader and more entwined with other areas of the development cycle than ever before. As such we bring different areas of expertise together for one goal: shaping products, services and interaction for the better.</p>
+            
+            <p>We work at the intersection of design, engineering and development with a focus of visualizing complex products and ideas. The means we employ depend on the project and range from branding new ventures to building interactive product visualizations for VR. Either inhouse or in close collaboration with our clients.</p>
+            
+            <p>To achieve this we prefer enduring relationships with deeper insight and accompanying projects from concept phase to rollout.</p>
+    
+            <p>If you have a project coming, feel free to hit us up!</p>
+        </section>
+    
+        <section class="credits">
+                <div class="credits-lists">
+                    <ul class="credits-list">
+                        <li class="credits-title">Design</li>
+                        <li class="credits-credit">Branding</li>
+                        <li class="credits-credit">Analysis</li>
+                        <li class="credits-credit">Good old Graphic Design</li>
+                    </ul>
+                    <ul class="credits-list">
+                        <li class="credits-title">3D Visualization</li>
+                        <li class="credits-credit">3D Concept Design</li>
+                        <li class="credits-credit">Texturing, Lighting &amp; Rendering</li>
+                        <li class="credits-credit">Animation</li>
+                        <li class="credits-credit">Interactive Visualization &amp; VR</li>
+                    </ul>
+                    <ul class="credits-list">
+                        <li class="credits-title">Web Development</li>
+                        <li class="credits-credit">Webdesign</li>
+                        <li class="credits-credit">Interface Design (GUI &amp; HID)</li>
+                        <li class="credits-credit">Programming</li>
+                    </ul>
+                </div>
+                <div class="credits-links">
+                    <ul class="credits-list credits-list--end">
+                        <li class="credits-title">Links</li>
+                        <li class="credits-link"><a target="_blank" href="index.php">Back to Home <img class="credits-arrow" src="../img/site/right-arrow.svg"></a></li>
+                    </ul>
+                </div>
+        </section>
+    
+        <section class="findmeon index">
+            <ul>
+                <li><a href="https://www.instagram.com/studioisphording/">Instagram</a></li>
+                <li><a href="https://dribbble.com/isphording">Dribble</a></li>
+                <li><a href="https://studioisphording.tumblr.com/">Tumblr</a></li>
+                <li><a href="https://twitter.com/jisphording">Twitter</a></li>
+                <li><a href="https://www.linkedin.com/in/johannes-isphording-622033139/">LinkedIn</a></li>
+            </ul>
+        </section>
+        
+        <!-- Add your site or application content here
+        <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. 
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='https://www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script> -->
+
+    <?php include $_SERVER['DOCUMENT_ROOT']."/inc/footer_projects.php"; ?>
+    
+</html>
