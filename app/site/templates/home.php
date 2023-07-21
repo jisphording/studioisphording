62e7e69919a5e9dbcd44bdc681090d4ec17f5ae3
jisphording <mail@johannesisphording.de>
2023-07-21 17:25:30 +0200

    [template] move showreel to top

--- app/site/templates/home.php
+++ app/site/templates/home.php
@@ -1,20 +1,20 @@
 <?php snippet('header') ?>
 
+	<!-- SHOWREEL -->
+	<section class="showreel">
+		<video playsinline autoplay muted loop poster="<?= $site->uri() . '/content/home/' . $page->showreel() ?>.jpg">
+			<?php print_r($page->videos()) ?>
+			<source src="<?= $site->uri() . 'home/' . $page->showreel() ?>.webm" type="video/webm" />
+			<source src="<?= $site->uri() . 'home/' . $page->showreel() ?>.ogg" type="video/ogg" />
+			Sorry, your browser doesn't support embedded videos, but don't worry, you can <a href="<?= $site->url() . 'home/' . $page->showreel() ?>.mp4">download it</a>
+			and watch it with your favorite video player!
+		</video>
+		<!-- Showreel Headline -->
+		<h1 class="showreel--title"><?= $page->title() ?></h1>
+	</section>
+
+	<!-- MAIN -->
 	<main class="section__main">
-
-		<!-- SHOWREEL -->
-		<section class="showreel">
-			<video playsinline autoplay muted loop poster="<?= $site->uri() . '/content/home/' . $page->showreel() ?>.jpg">
-				<?php print_r($page->videos()) ?>
-				<source src="<?= $site->uri() . 'home/' . $page->showreel() ?>.webm" type="video/webm" />
-				<source src="<?= $site->uri() . 'home/' . $page->showreel() ?>.ogg" type="video/ogg" />
-				Sorry, your browser doesn't support embedded videos, but don't worry, you can <a href="<?= $site->url() . 'home/' . $page->showreel() ?>.mp4">download it</a>
-				and watch it with your favorite video player!
-			</video>
-			<!-- Showreel Headline -->
-			<h1 class="showreel--title"><?= $page->title() ?></h1>
-		</section>
-
 		<section class="section__bodycopy">
 			<p><?= $page->outro() ?></p>
 		</section>
