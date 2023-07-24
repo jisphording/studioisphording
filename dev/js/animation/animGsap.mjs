e307b7a729679df17d63e2bbbec5d73c7a1f7db7
jisphording <mail@johannesisphording.de>
2023-07-25 00:15:52 +0200

    [css][gsap] changed the scrolltrigger

--- dev/js/animation/animGsap.mjs
+++ dev/js/animation/animGsap.mjs
@@ -9,17 +9,31 @@ export function animGsap() {
 
     gsap.registerPlugin( ScrollTrigger );
 
-    // PARALLAX HEADER IMG
+    // Connect Parallax Layers
     let parallaxBack = document.getElementsByClassName( 'parallax__layer--back' )[ 0 ]
-    // PARALLAX FRONT
+    let parallaxTitle = document.getElementsByClassName( 'parallax__layer--title' )[ 0 ]
     let parallaxFront = document.getElementsByClassName( 'parallax__layer--base' )[ 0 ]
-    // PARALLAX TRIGGER
     let parallaxTrigger = document.getElementsByClassName( 'parallax' )[ 0 ]
 
-    gsap.to( parallaxBack, {
-        yPercent: 200,
+    gsap.to( parallaxBack, 
+    {
+        yPercent: 0,
         ease: 'none',
-        scrollTrigger: {
+        scrollTrigger: 
+        {
+            trigger: parallaxTrigger,
+            start: 'top bottom',
+            end: 'bottom top',
+            scrub: true
+        }
+    })
+
+    gsap.to( parallaxTitle, 
+    {
+        scale: 1,
+        ease: 'none',
+        scrollTrigger: 
+        {
             trigger: parallaxTrigger,
             start: 'top bottom',
             end: 'bottom top',
