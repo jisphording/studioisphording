9e39597431e79484f466a07410f21f11a6ec660d
jisphording <mail@johannesisphording.de>
2023-07-24 14:25:08 +0200

    [template] add parallax scrolling to home

--- app/site/templates/home.php
+++ app/site/templates/home.php
@@ -1,38 +1,44 @@
 <?php snippet('header') ?>
+	<section class="parallax">
 
-	<!-- SHOWREEL -->
-	<section class="showreel">
-		<video playsinline autoplay muted loop poster="<?= $site->uri() . '/content/home/' . $page->showreel() ?>.jpg">
-			<?php print_r($page->videos()) ?>
-			<source src="<?= $site->uri() . 'home/' . $page->showreel() ?>.webm" type="video/webm" />
-			<source src="<?= $site->uri() . 'home/' . $page->showreel() ?>.ogg" type="video/ogg" />
-			Sorry, your browser doesn't support embedded videos, but don't worry, you can <a href="<?= $site->url() . 'home/' . $page->showreel() ?>.mp4">download it</a>
-			and watch it with your favorite video player!
-		</video>
-		<!-- Showreel Headline -->
-		<h1 class="showreel--title"><?= $page->title() ?></h1>
-	</section>
-
-	<!-- MAIN -->
-	<main class="section__main">
-		<section class="section__bodycopy">
-			<p><?= $page->outro() ?></p>
+		<!-- SHOWREEL -->
+		<section class="showreel">
+			<section class="parallax__layer--back">
+				<video playsinline autoplay muted loop poster="<?= $site->uri() . '/content/home/' . $page->showreel() ?>.jpg">
+					<?php print_r($page->videos()) ?>
+					<source src="<?= $site->uri() . 'home/' . $page->showreel() ?>.webm" type="video/webm" />
+					<source src="<?= $site->uri() . 'home/' . $page->showreel() ?>.ogg" type="video/ogg" />
+					Sorry, your browser doesn't support embedded videos, but don't worry, you can <a href="<?= $site->url() . 'home/' . $page->showreel() ?>.mp4">download it</a>
+					and watch it with your favorite video player!
+				</video>
+				<!-- Showreel Headline -->
+				<h1 class="showreel--title"><?= $page->title() ?></h1>
+			</section>
 		</section>
 
-		<!-- PROJECT SHOWCASE -->
-		<section class="projects__showcase">
+		<!-- MAIN -->
+		<main class="section__main parallax__layer--base">
+			<section class="section__bodycopy">
+				<p><?= $page->outro() ?></p>
+			</section>
 
-			<!-- Showcase Loop -->
-			<?php $site->displayShowcase('projects', 4) ?>
+			<!-- PROJECT SHOWCASE -->
+			<section class="projects__showcase">
 
-			<!-- Link to all projects -->
-			<p class="projects__showcase--more">
-				<a href="<?= page('projects')->url() ?>" class="btn"><?php echo t('Show all projects') ?></a>
-			</p>
+				<!-- Showcase Loop -->
+				<?php $site->displayShowcase('projects', 4) ?>
 
-		</section>
+				<!-- Link to all projects -->
+				<p class="projects__showcase--more">
+					<a href="<?= page('projects')->url() ?>" class="btn"><?php echo t('Show all projects') ?></a>
+				</p>
 
-	</main>
-	</body>
+			</section>
+
+		</main>
 
 	<?php snippet('footer') ?>
+
+	</section><!-- End: Parallax -->
+
+	</body>
