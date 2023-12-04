4242d3ead1c49aa6dc4fd72c3303c3b2d76e3bbe
Johannes Isphording <mail@johannesisphording.de>
2023-12-04 23:36:30 +0100

    [three][sizes] manage viewport resizing

---
+++ dev/js/three/utils/Sizes.mjs
@@ -0,0 +1,33 @@
+// ---------- ---------- ---------- ---------- ---------- //
+// S I Z E S //
+// ---------- ---------- ---------- ---------- ---------- //
+//
+// Handling the resizing of the threejs canvas.
+
+import EventEmitter from './EventEmitter.mjs'
+
+export class Sizes extends EventEmitter
+{
+    constructor( canvas )
+    {
+        super()
+
+        // SETUP
+        this.width = canvas.offsetWidth
+        this.height = canvas.offsetHeight
+        
+        // Limiting the pixel ratio for performance reasons
+        this.pixelRatio = Math.min( window.devicePixelRatio, 2 )
+
+        // RESIZE EVENT
+        window.addEventListener( 'resize', () =>
+        {
+            this.width = canvas.parentNode.offsetWidth
+            this.height = canvas.parentNode.offsetHeight
+            this.pixelRatio = Math.min( window.devicePixelRatio, 2 )
+
+            // NOTIFY EXPERIENCE
+            this.trigger( 'resize' )
+        })
+    }
+}
