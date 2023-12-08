0c9d479653b301066d8565a27918281c29f519f3
jisphording <mail@johannesisphording.de>
2023-12-08 20:36:58 +0100

    [three][asset] add 3d mesh

---
+++ dev/js/three/projects/isphording-inneneinrichtung/DisplayAppleXDR.mjs
@@ -0,0 +1,42 @@
+// Import external Libraries
+import * as THREE from 'three'
+
+// Import Modules
+import { Experience } from "./../../modules/Experience.mjs"
+
+// ---------- ---------- ---------- ---------- ---------- //
+// D I S P L A Y   A P P L E   X D R //
+// ---------- ---------- ---------- ---------- ---------- //
+
+export class DisplayAppleXDR
+{
+    constructor()
+    {
+        this.experience = new Experience()
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+
+        // Retrieve correct resource
+        this.resource = this.resources.items.DisplayAppleXDR
+    
+        this.setModel()
+    }
+
+    setModel()
+    {
+        this.model = this.resource.scene
+
+        // Center Pivot
+        const bbox = new THREE.Box3().setFromObject( this.model )
+        const offset = new THREE.Vector3()
+        bbox.getCenter( offset ).negate()
+        this.model.children[0].position.set( offset.x, offset.y, offset.z )
+
+        // Custom Scene Transforms
+        this.model.scale.set( 2, 2, 2)
+        this.model.rotation.y += 180
+
+        // Add to Scene
+        this.scene.add( this.model )
+    }
+}
