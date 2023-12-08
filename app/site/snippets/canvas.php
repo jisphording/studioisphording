3fa464e2556c6d107c1d34ae888e5f827f25af4e
jisphording <mail@johannesisphording.de>
2023-12-08 20:31:54 +0100

    [template] add canvas header snippet

---
+++ app/site/snippets/canvas.php
@@ -0,0 +1,13 @@
+<!-- WEBGL CANVAS -->
+<section class="showreel">
+	<!-- Showreel Title -->
+	<div class="showreel__title--wrapper parallax__layer--title">
+		<div class="showreel__title">
+			<h1><?= $page->titlelong() ?></h1>
+		</div>
+	</div>
+	<!-- Intro Image -->
+	<section class="showreel__video parallax__layer--back">
+        <canvas id="webgl" class="showreel__video parallax__layer--back showcase__intro__image"></canvas>
+	</section>
+</section>
