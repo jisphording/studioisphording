1c61dc442071d4c18878aa0c598631e95d9c3ac6
jisphording <mail@johannesisphording.de>
2024-02-04 02:05:28 +0100

    [template] updating footer message

--- app/site/snippets/footer.php
+++ app/site/snippets/footer.php
@@ -1,14 +1,19 @@
-	<footer class="footer section__main menu">
-		<ul>
-			<li><!-- EMPTY	 --></li>
-		</ul>
-		<ul>
-			<p class="copyright"><?= $site->copyright()->html() ?></p>
-			<li><a href="<?= $pages->find('imprint')->url() ?>">Imprint</a></li>
-			<li><a href="<?= $pages->find('disclaimer')->url() ?>">Disclaimer</a></li>
-			<li><a href="<?= $pages->find('privacy')->url() ?>">Data & Privacy Policy</a></li>
-			<li><a href="<?= $pages->find('gdpr')->url() ?>">GDPR</a></li>
-		</ul>
+	<footer class="footer section__main">
+		<section class="footer__quote article__main">
+			<p>Studio Isphording entwickelt Design für Print und Digitale Medien und setzt dabei auf Technologien der 3D Visualisierung, Virtual Reality und Generativer AI.</p>
+		</section>
+		<section class="menu">
+			<ul>
+				<li><!-- EMPTY	 --></li>
+			</ul>
+			<ul>
+				<p class="copyright"><?= $site->copyright()->html() ?></p>
+				<li><a href="<?= $pages->find('imprint')->url() ?>">Imprint</a></li>
+				<li><a href="<?= $pages->find('disclaimer')->url() ?>">Disclaimer</a></li>
+				<li><a href="<?= $pages->find('privacy')->url() ?>">Data & Privacy Policy</a></li>
+				<li><a href="<?= $pages->find('gdpr')->url() ?>">GDPR</a></li>
+			</ul>
+		</section>
 	</footer>
 
 	<?= js([
