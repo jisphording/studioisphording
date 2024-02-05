f3f998c3cd0e1c30d06fe4e6955fc9f75bdf4887
jisphording <mail@johannesisphording.de>
2024-02-05 01:07:58 +0100

    [template] remove wrong class

--- app/site/snippets/intro-img.php
+++ app/site/snippets/intro-img.php
@@ -11,7 +11,7 @@
 		<?php $introimg = $page->images()->filterBy('filename', '*=', 'intro-img')->first(); 
 		if ( !empty( $introimg )) { ?>
 			<!-- Image Wrapper -->
-			<figure class="showreel__video parallax__layer--back showcase__intro__image">
+			<figure class="showcase__intro__image">
 				<!-- TODO - The way the following img code is implemented is used so frequently on this website that it
 							is probably a good idea to roll it into it's own function -->
 				<img srcset="<?= $introimg -> srcset([480, 768, 1024, 1280, 1440, 1680, 1920, 2560, 3840]) ?>"
