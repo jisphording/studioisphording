f7d9a7cbe05f75db4409ebe8dbc44c710ec1641a
jisphording <mail@johannesisphording.de>
2024-02-12 15:08:17 +0100

    [css] adding a mobile menu

--- app/site/snippets/menu-main.php
+++ app/site/snippets/menu-main.php
@@ -12,6 +12,26 @@ if ($items->isNotEmpty()) :
 		<a href="<?= $site->url() ?>">Isphording</a>
 	</section>
 
+	<!-- MOBILE MENU -->
+	<nav class="menu menu__mobile">
+		<div class="menu__mobile__toggle__wrapper">
+			<label for="menu__mobile__toggle" class="menu__mobile__toggle">
+				<input type="checkbox" />
+			</label>
+			
+			<aside class="sidebar">
+			<ul>
+				<?php foreach ($items as $item) : ?>
+					<li>
+						<a<?php e($item->isOpen(), ' class="active"') ?> href="<?= $item->url() ?>"><?= $item->title()->html() ?></a>
+					</li>
+				<?php endforeach ?>
+			</ul>
+		</aside>
+		</div>
+	</nav>
+
+	<!-- DESKTOP MENU -->
 	<nav class="menu menu__main">
 		<ul>
 			<?php foreach ($items as $item) : ?>
