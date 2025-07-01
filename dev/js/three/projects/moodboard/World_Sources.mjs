af4cc111a85cdf708896781e834ae5eb07e18592
Johannes Isphording <mail@johannesisphording.de>
2025-07-02 00:46:59 +0200

    [three][moodboard] loading ressources

---
+++ dev/js/three/projects/moodboard/World_Sources.mjs
@@ -0,0 +1,54 @@
+// S O U R C E S
+/* ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- */
+//
+// Storage Array
+
+const moodboardFolder = '/content/moodboard/';
+
+export default [
+    // Moodboard Images
+    { name: 'moodboardImage_ea9f8336688c41762b6ce4982a3e4f08e4c63730', type: 'texture', path: moodboardFolder + 'ea9f8336688c41762b6ce4982a3e4f08e4c63730.png' },
+    { name: 'moodboardImage_HSR_STILLS_16x9_4K_18-1536x864', type: 'texture', path: moodboardFolder + 'HSR_STILLS_16x9_4K_18-1536x864.webp' },
+    { name: 'moodboardImage_img_2_1543930141_057bed4730bc963c2c29ca4dc77fb8ec', type: 'texture', path: moodboardFolder + 'img_2_1543930141_057bed4730bc963c2c29ca4dc77fb8ec.webp' },
+    { name: 'moodboardImage_img_2_1545022143_86cf9c637b9944c746b16ad4f0d226b7', type: 'texture', path: moodboardFolder + 'img_2_1545022143_86cf9c637b9944c746b16ad4f0d226b7.webp' },
+    { name: 'moodboardImage_img_3_1537874577_54abb025a489e9ce811a40b5a5ba1c17', type: 'texture', path: moodboardFolder + 'img_3_1537874577_54abb025a489e9ce811a40b5a5ba1c17.webp' },
+    { name: 'moodboardImage_img_3_1540395586_a1130ddd13f69edbd5fba45ebb0aa489', type: 'texture', path: moodboardFolder + 'img_3_1540395586_a1130ddd13f69edbd5fba45ebb0aa489.webp' },
+    { name: 'moodboardImage_img_3_1540494080_4c142d6d65990990f27bdaf7c17224df', type: 'texture', path: moodboardFolder + 'img_3_1540494080_4c142d6d65990990f27bdaf7c17224df.webp' },
+    { name: 'moodboardImage_img_5_1540395586_af4086ba44589cb444fe2faa0016f3ae', type: 'texture', path: moodboardFolder + 'img_5_1540395586_af4086ba44589cb444fe2faa0016f3ae.webp' },
+    { name: 'moodboardImage_img_5_1540564516_89b8d1bf91fcdf01269add01aa4a6a8f', type: 'texture', path: moodboardFolder + 'img_5_1540564516_89b8d1bf91fcdf01269add01aa4a6a8f.webp' },
+    { name: 'moodboardImage_img_9_1545022143_a81978ab07ac45ccb4ef92e222905991', type: 'texture', path: moodboardFolder + 'img_9_1545022143_a81978ab07ac45ccb4ef92e222905991.webp' },
+    { name: 'moodboardImage_img_12_1545055065_e197e82ea3401019c992dbc273791359', type: 'texture', path: moodboardFolder + 'img_12_1545055065_e197e82ea3401019c992dbc273791359.webp' },
+    { name: 'moodboardImage_Jeil_Health_Science_graphic_identity_logo_design_graphic_blog_project_mindsparkle_mag_beautiful_portfolio_8fa249109290765_5fd094e069f4b', type: 'texture', path: moodboardFolder + 'Jeil-Health-Science-graphic-identity-logo-design-graphic-blog-project-mindsparkle-mag-beautiful-portfolio-8fa249109290765.5fd094e069f4b.jpg.webp' },
+    { name: 'moodboardImage_jxlyjk14luyt', type: 'texture', path: moodboardFolder + 'jxlyjk14luyt.webp' },
+    { name: 'moodboardImage_lorenzo_mariotti_electric_tractor_concept_designboom_1', type: 'texture', path: moodboardFolder + 'lorenzo-mariotti-electric-tractor-concept-designboom-1.webp' },
+    { name: 'moodboardImage_matteucci_garage_dakhla_honda_nx650_motorcycle_designboom_1', type: 'texture', path: moodboardFolder + 'matteucci-garage-dakhla-honda-nx650-motorcycle-designboom-1.webp' },
+    { name: 'moodboardImage_miramacho_website_esntial_design_01_e8e328d778', type: 'texture', path: moodboardFolder + 'miramacho_website_esntial_design_01_e8e328d778.webp' },
+    { name: 'moodboardImage_oke_by_thought_and_found_esntial_design_01_76d08c80c4', type: 'texture', path: moodboardFolder + 'oke_by_thought_and_found_esntial_design_01_76d08c80c4.webp' },
+    { name: 'moodboardImage_pangram_pangram_bureau_borsche_o_format_webp_width_2880_bS2OaXl2MZOj43Kd', type: 'texture', path: moodboardFolder + 'pangram-pangram-bureau-borsche-o.format-webp.width-2880_bS2OaXl2MZOj43Kd.webp' },
+    { name: 'moodboardImage_Paw_Plex_design_logo_design_graphic_blog_project_mindsparkle_mag_beautiful_portfolio_bb88ca109400683_5fd2b1305d56a', type: 'texture', path: moodboardFolder + 'Paw-Plex-design-logo-design-graphic-blog-project-mindsparkle-mag-beautiful-portfolio-bb88ca109400683.5fd2b1305d56a.jpg.webp' },
+    { name: 'moodboardImage_Paw_Plex_design_logo_design_graphic_blog_project_mindsparkle_mag_beautiful_portfolio_f5f707109400683_5fd2b1305e200', type: 'texture', path: moodboardFolder + 'Paw-Plex-design-logo-design-graphic-blog-project-mindsparkle-mag-beautiful-portfolio-f5f707109400683.5fd2b1305e200.jpg.webp' },
+    { name: 'moodboardImage_projects', type: 'texture', path: moodboardFolder + 'projects.webp' },
+    { name: 'moodboardImage_rattan_in_concrete_jungle_4_5ed24e6dcdf8f', type: 'texture', path: moodboardFolder + 'rattan-in-concrete-jungle-4-5ed24e6dcdf8f.webp' },
+    { name: 'moodboardImage_roihan_haidar_el_LuI0mVLo_unsplash', type: 'texture', path: moodboardFolder + 'roihan-haidar-el-LuI0mVLo-unsplash.jpg' },
+    { name: 'moodboardImage_ronin_47_blacked_out_motorcycle_designboom_1', type: 'texture', path: moodboardFolder + 'ronin-47-blacked-out-motorcycle-designboom-1.webp' },
+    { name: 'moodboardImage_screencapture_youtube_watch_2025_06_18_23_33_23', type: 'texture', path: moodboardFolder + 'screencapture-youtube-watch-2025-06-18-23_33_23.png' },
+    { name: 'moodboardImage_ShoppableImage2_TheKitchen_1920x', type: 'texture', path: moodboardFolder + 'ShoppableImage2_TheKitchen_1920x.webp' },
+    { name: 'moodboardImage_standard_projects_devrev_digital_402', type: 'texture', path: moodboardFolder + 'standard-projects-devrev-digital-402.webp' },
+    { name: 'moodboardImage_static_wixstatic_com_14217356368564028', type: 'texture', path: moodboardFolder + 'static.wixstatic.com-14217356368564028.webp' },
+    { name: 'moodboardImage_stator_electric_scooter_self_balancing_designboom_2', type: 'texture', path: moodboardFolder + 'stator-electric-scooter-self-balancing-designboom-2.webp' },
+    { name: 'moodboardImage_studio_oker_portfolio_website_the_essential_design_2_f548e83102', type: 'texture', path: moodboardFolder + 'studio_oker_portfolio_website_the_essential_design_2_f548e83102.webp' },
+    { name: 'moodboardImage_studio_oker_portfolio_website_the_essential_design_6_b2ab5dd958', type: 'texture', path: moodboardFolder + 'studio_oker_portfolio_website_the_essential_design_6_b2ab5dd958.webp' },
+    { name: 'moodboardImage_studio_WOK_country_home_chievo_designboom_014', type: 'texture', path: moodboardFolder + 'studio-WOK-country-home-chievo-designboom-014.webp' },
+    { name: 'moodboardImage_study_by_shifwalk_the_essential_design_15_64a73ea0d2', type: 'texture', path: moodboardFolder + 'study_by_shifwalk_the_essential_design_15_64a73ea0d2.webp' },
+    { name: 'moodboardImage_supergrand_by_caserne_the_essential_design_7_eba8dbdb30', type: 'texture', path: moodboardFolder + 'supergrand_by_caserne_the_essential_design_7_eba8dbdb30.webp' },
+    { name: 'moodboardImage_tabla_rasa_by_estudioblende_esntial_design_09_464a26bb67', type: 'texture', path: moodboardFolder + 'tabla_rasa_by_estudioblende_esntial_design_09_464a26bb67.webp' },
+    { name: 'moodboardImage_tbvd_samples1_1536x918', type: 'texture', path: moodboardFolder + 'tbvd-samples1-1536x918.webp' },
+    { name: 'moodboardImage_tbvd_table1_1536x1901', type: 'texture', path: moodboardFolder + 'tbvd-table1-1536x1901.webp' },
+    { name: 'moodboardImage_Tendril_Logo_Structure_01', type: 'texture', path: moodboardFolder + 'Tendril_Logo_Structure_01.webp' },
+    { name: 'moodboardImage_The_taste_therapy_brand_branding_visual_identity_design_graphic_hand_drawn_rustic_warm_illustration_line_Mindsparkle_Mag5', type: 'texture', path: moodboardFolder + 'The-taste-therapy-brand-branding-visual-identity-design-graphic-hand-drawn-rustic-warm-illustration-line-Mindsparkle-Mag5.jpg.webp' },
+    { name: 'moodboardImage_Tshirt_01_v001_0000_scaled_e1660233379355_1536x1536', type: 'texture', path: moodboardFolder + 'Tshirt_01_v001_0000-scaled-e1660233379355-1536x1536.webp' },
+    { name: 'moodboardImage_v48srxuhlsshgewjaixb', type: 'texture', path: moodboardFolder + 'v48srxuhlsshgewjaixb.webp' },
+    { name: 'moodboardImage_Valha_fashion_visual_identity_brand_branding_design_typography_custom_modern_clean_young_fresh_glam_Mindsparkle_Mag', type: 'texture', path: moodboardFolder + 'Valha-fashion-visual-identity-brand-branding-design-typography-custom-modern-clean-young-fresh-glam-Mindsparkle-Mag.jpg.webp' },
+    { name: 'moodboardImage_vinyl_records_by_tomorrow_the_essential_design_4_439753b55c', type: 'texture', path: moodboardFolder + 'vinyl_records_by_tomorrow_the_essential_design_4_439753b55c.webp' },
+    { name: 'moodboardImage_volksans_3_6', type: 'texture', path: moodboardFolder + 'volksans-3-6.webp' }
+]
