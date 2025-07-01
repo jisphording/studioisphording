5130f4a909f2e09cc096f6a6b1766745d4747e37
Johannes Isphording <mail@johannesisphording.de>
2025-07-02 01:02:37 +0200

    [template][mood] minimal canvas setup for testing

---
+++ app/site/snippets/canvas-minimal.php
@@ -0,0 +1,8 @@
+<!-- WEBGL CANVAS -->
+<section class="canvas-minimal">
+    <canvas id="webgl" class="canvas-minimal" 
+	    data-world="<?= $page->slug() == 'isphording-inneneinrichtung' ? 'World_01' : 'World_02' ?>"
+        style="z-index: 9;"
+    >
+    </canvas>
+</section>
--- app/site/templates/moodboard.php
+++ app/site/templates/moodboard.php
@@ -1,24 +1,7 @@
 <?php snippet('header') ?>
-	<section class="parallax">
+	
+	<?php snippet('canvas-minimal') ?>
+	
+	<?php echo vite('dev/js/index.js') ?>
 
-		<?php snippet('canvas') ?>
-
-		<!-- PROJECT SINGLE VIEW MAIN CONTENT -->
-		<main class="section__main project__single parallax__layer--base" role="main">
-			
-			<!-- PROJECT CONTENT -->
-			<div class="project__single--content">
-
-				<!-- Project Description  -->
-				<h3><?php echo t('Project Description') ?></h3>
-
-			</div>
-			<!-- END - PROJECT CONTENT -->
-		</main>
-		<!-- END - PROJECT SINGLE VIEW MAIN CONTENT -->
-	</section>
-	<!-- END - PARALLAX -->
-
-    <?php echo vite('dev/js/index.js') ?>
-
-<?php snippet('footer') ?>
+	<?php snippet('footer') ?>
