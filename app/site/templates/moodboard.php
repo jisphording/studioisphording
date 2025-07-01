e7066b0698ac450b78117496146050d2f88deeda
Johannes Isphording <mail@johannesisphording.de>
2025-07-02 00:26:20 +0200

    [kirby][template] added a new moodboard template

---
+++ app/site/templates/moodboard.php
@@ -0,0 +1,30 @@
+<?php snippet('header') ?>
+	<section class="parallax">
+
+		<?php snippet('canvas') ?>
+
+		<!-- PROJECT SINGLE VIEW MAIN CONTENT -->
+		<main class="section__main project__single parallax__layer--base" role="main">
+			
+			<!-- PROJECT CONTENT -->
+			<div class="project__single--content">
+
+				<!-- Project Description  -->
+				<h3><?php echo t('Project Description') ?></h3>
+
+				<!-- PROJECT GALLERY -->
+				<?php snippet('project-gallery', [
+					'videoPath' => 'app/video/',
+					'useResponsiveImages' => true
+				]) ?>
+
+			</div>
+			<!-- END - PROJECT CONTENT -->
+		</main>
+		<!-- END - PROJECT SINGLE VIEW MAIN CONTENT -->
+	</section>
+	<!-- END - PARALLAX -->
+
+    <?php echo vite('dev/js/index.js') ?>
+
+<?php snippet('footer') ?>
